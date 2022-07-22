3bad7c478ac395cb86b52b85e529112218acde42
Budi Prasetyo <b.prasetyo@eksad.com>
2022-07-22 10:05:59 +0700

    PROTALENT  - BUDI
    - Container client

--- lib/admin/client_dashboard.dart
+++ lib/admin/client_dashboard.dart
@@ -5,6 +5,43 @@ class ClientDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Client'),);
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Container(
+          //padding: EdgeInsets.symmetric(vertical: screenSize.height*0.1,horizontal: screenSize.width*0.2),
+          width: screenSize.width*0.7,
+          height: screenSize.height*0.7,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.blue,width: 5)
+            //color: Colors.blue,
+          ),
+
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20)
+                  ),
+
+                ),
+                width: screenSize.width*0.7,
+                height: screenSize.height*0.15,
+                
+              ),
+              Container(width: screenSize.width*0.7,
+                height: screenSize.height*0.025,),
+              Container(
+
+                width: screenSize.width*0.65,
+                height: screenSize.height*0.5,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
