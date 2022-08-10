61daf96bd0ac3e5285fa38e860c4fb83a7e1fa58
Fikri <adhefikri98@gmail.com>
2022-08-10 14:14:55 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "PROTALENT BY EKSAD",
+
       home: Scaffold(
         body: HomePage(),
       ),
