478041ed2d67127afcd8acf16ef9e736ae5d67c4
rizky ramadhan <r.ramadhan@gmail.com>
2022-08-10 16:45:33 +0700

    career3

--- lib/public_baru/career/career.dart
+++ lib/public_baru/career/career.dart
@@ -1,12 +1,12 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:pro_talent/public_baru/career/career3.dart';
 
 import '../../appbar/appbar_baru.dart';
 import '../../public/footer.dart';
 import '../../widget/responsive.dart';
 import '../../widget/whatsapp.dart';
 import 'career1.dart';
+import 'career3.dart';
 
 class Career extends StatefulWidget {
   const Career({Key? key}) : super(key: key);
@@ -22,17 +22,16 @@ class _CareerState extends State<Career> {
     return Scaffold(
         appBar: ResponsiveWidget.isSmallScreen(context)
             ? AppbarHomeSmall(screenSize)
-            : AppbarHomeLarge(screenSize, context, Colors.blue, Colors.black,Colors.black,
-            Colors.black, Colors.black),
+            : AppbarHomeLarge(screenSize, context, Colors.blue, Colors.black,
+                Colors.black, Colors.black, Colors.black),
         body: ListView(
           children: [
-
             Career1(),
+            Career3(),
             Footer(),
           ],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: WAChat()
-    );
+        floatingActionButton: WAChat());
   }
 }
